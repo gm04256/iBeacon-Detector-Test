2f4a4ebae97d5efc5c7a3f259c1ba7e9017f696b
Ma Yan <896098846@qq.com>
2014-06-23 20:17:32 +0900

    Project Created

---
+++ iBeacon Detector Test/iBeacon Detector TestTests/iBeacon_Detector_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  iBeacon_Detector_TestTests.m
+//  iBeacon Detector TestTests
+//
+//  Created by 馬 岩 on 14-6-23.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iBeacon_Detector_TestTests : XCTestCase
+
+@end
+
+@implementation iBeacon_Detector_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
